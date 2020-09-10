269d6735a4424ef6f015b8e8702623f5a957c76c
Julia Cavazos-England <jacob.e.england@gmail.com>
2020-09-10 16:18:13 -0400

    Adding untracked

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
-live = '![live](https://i.ibb.co/VJ5mML7/live.png)'
-dead = '![dead](https://i.ibb.co/x3MGSVW/dead.png)'
-
-map = [
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-    [1, 1, 0, 1, 1],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-]
 
 // Data to update
 let DATA = {
@@ -25,24 +15,6 @@ let DATA = {
         timeZone: 'America/New_York'
       }),
 };
-
-let i = 0
-
-for (let i = 0; i < map.length; i++) {
-    let cells = ''
-    row = map[i]
-    for (let j = 0; j < row.length; j++) {
-        state = row[j]
-        if (state == 1) {
-            cells += live
-        } else {
-            cells += dead
-        }
-    }
-    cells += '  '
-    console.log(cells)
-    DATA[`row${i}`] = cells
-}
 
 function generateREADME() {
     fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
