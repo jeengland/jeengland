170eac52878d94023809b88f93697777f23b678d
Julia Cavazos-England <jacob.e.england@gmail.com>
2020-09-10 13:21:19 -0400

    Added generation logic and reworked generator to work with array data

---
+++ mapLogic.js
@@ -0,0 +1,13 @@
+const createMap = require('./createMap.js');
+const randomizeMap = require('./randomizeMap.js');
+const generation = require('./generation.js');
+
+const map = randomizeMap(10, 10);
+const color = [255, 0, 0];
+
+createMap(map, color);
+
+for (let i = 0; i < 10; i++) {
+    map = generation(map)
+    createMap(map, color, `map${i}.png`)
+}
