fa64b99d836fa3caceaedf9c1c2d28de77e19b73
Julia Cavazos-England <jacob.e.england@gmail.com>
2020-09-10 16:16:31 -0400

    Merge

--- mapUtils/mapLogic.js
+++ mapUtils/mapLogic.js
@@ -4,41 +4,41 @@ const generation = require('./generation.js');
 const readHistory = require('./readHistory.js');
 const writeHistory = require('./writeHistory.js');
 
-// Pull data from historical.json
-const { data } = readHistory();
+const mapLogic = (color) => {
+    // Pull data from historical.json
+    const { data } = readHistory();
 
-!(data.length % 5) && console.log(data.length)
+    !(data.length % 5) && console.log(data.length)
 
-// Generate a random map if there is are no maps in data
-if (data.length == 0) {
-    // The structure an entry in the historical data is { map, mapDate }
-    // where map is an array of values and mapDate is the date it was created
-    let map = randomizeMap(100, 100);
-    let mapDate = new Date().toString();
-    data.push({ map, mapDate });
-};
+    // Generate a random map if there is are no maps in data
+    if (data.length == 0) {
+        // The structure an entry in the historical data is { map, mapDate }
+        // where map is an array of values and mapDate is the date it was created
+        let map = randomizeMap(100, 100);
+        let mapDate = new Date().toString();
+        data.push({ map, mapDate });
+    };
 
-// Create a shallow copy of the latest map
-let map = [...data[data.length - 1].map];
-// `color` is the color of the live cells in the generated images
-const color = [255, 0, 0];
-// `max` is the maximum allowed entries in historical.json
-const max = 100;
+    // Create a shallow copy of the latest map
+    let map = [...data[data.length - 1].map];
+    // `max` is the maximum allowed entries in historical.json
+    const max = 100;
 
-// Generate the next stage of the game of life using the latest map
-map = generation(map);
-mapDate = new Date().toString();
+    // Generate the next stage of the game of life using the latest map
+    map = generation(map);
+    mapDate = new Date().toString();
 
-// Add the map to the historical data
-data.push({ map, mapDate })
+    // Add the map to the historical data
+    data.push({ map, mapDate })
 
-// Create the map image
-createMap(map, color);
+    // Create the map image
+    createMap(map, color);
 
-// Ensure that history doesn't have any more entries than it should
-while (data.length > max) {
-    data.shift();
-};
+    // Ensure that history doesn't have any more entries than it should
+    while (data.length > max) {
+        data.shift();
+    };
 
-// Store the new data in historical.json
-writeHistory(data);
+    // Store the new data in historical.json
+    writeHistory(data);
+}
