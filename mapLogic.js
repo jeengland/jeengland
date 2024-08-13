335314132a33b056a93814b043e29fe405cfc5a4
Julia Cavazos-England <jacob.e.england@gmail.com>
2024-08-13 16:45:14 -0400

    feat: add loop detection

--- mapLogic.js
+++ mapLogic.js
@@ -1,48 +1,86 @@
-const createMap = require('./createMap.js');
-const randomizeMap = require('./randomizeMap.js');
-const generation = require('./generation.js');
-const readHistory = require('./readHistory.js');
-const writeHistory = require('./writeHistory.js');
-const historyToGIF = require('./historyToGIF.js');
+const createMap = require("./createMap.js");
+const randomizeMap = require("./randomizeMap.js");
+const generation = require("./generation.js");
+const readHistory = require("./readHistory.js");
+const writeHistory = require("./writeHistory.js");
+const historyToGIF = require("./historyToGIF.js");
+
+const { createHash } = require("crypto");
+const detectLoop = require("./detectLoop.js");
 
 const mapLogic = (color) => {
-    // Pull data from historical.json
-    const { data } = readHistory();
+  // Pull data from historical.json
+  let { data } = readHistory();
 
-    // Generate a random map if there is are no maps in data
-    if (data.length == 0) {
-        // The structure an entry in the historical data is { map, mapDate }
-        // where map is an array of values and mapDate is the date it was created
-        let map = randomizeMap(100, 100);
-        let mapDate = new Date().toString();
-        data.push({ map, mapDate });
-    };
+  // Generate a random map if there is are no maps in data
+  if (data.length == 0) {
+    // The structure an entry in the historical data is { map, mapDate }
+    // where map is an array of values and mapDate is the date it was created
+    let newMap = randomizeMap(100, 100);
+    let mapDate = new Date().toString();
 
-    // Create a shallow copy of the latest map
-    let map = [...data[data.length - 1].map];
-    // `max` is the maximum allowed entries in historical.json
-    const max = 100;
+    data.push(generateMapData(newMap, mapDate));
 
-    // Generate the next stage of the game of life using the latest map
-    map = generation(map);
-    mapDate = new Date().toString();
+    for (let i = 0; i < 19; i++) {
+      newMap = generation(newMap);
+      data.push(generateMapData(newMap, mapDate));
+    }
+  }
 
-    // Add the map to the historical data
-    data.push({ map, mapDate })
+  // Create a shallow copy of the latest map
+  let map = [...data[data.length - 1].map];
+  // `max` is the maximum allowed entries in historical.json
+  const max = 100;
 
-    // Create the map image
-    createMap(map, color);
+  // Generate the next stage of the game of life using the latest map
+  map = generation(map);
+  mapDate = new Date().toString();
 
-    // Ensure that history doesn't have any more entries than it should
-    while (data.length > max) {
-        data.shift();
-    };
+  // Add the map to the historical data
+  data.push(generateMapData(map, mapDate));
 
-    // Store the new data in historical.json
-    writeHistory(data);
+  // Detect if a loop is present
+  const isLoop = detectLoop(data);
 
-    // Generate GIF based off new data
-    historyToGIF(data);
-}
+  if (isLoop) {
+    console.log("Loop detected, restarting map");
+
+    data = [];
+
+    let newMap = randomizeMap(100, 100);
+
+    data.push(generateMapData(newMap, mapDate));
+
+    for (let i = 0; i < 19; i++) {
+      newMap = generation(newMap);
+      data.push(generateMapData(newMap, mapDate));
+    }
+  }
+
+  // Create the map image
+  createMap(data[data.length - 1].map, color);
+
+  // Ensure that history doesn't have any more entries than it should
+  while (data.length > max) {
+    data.shift();
+  }
+
+  // Store the new data in historical.json
+  writeHistory(data);
+
+  // Generate GIF based off new data
+  historyToGIF(data);
+};
+
+const generateMapData = (map, mapDate) => {
+  const mapString = JSON.stringify(map);
+  const key = createHash("sha256").update(mapString).digest("hex");
+
+  return {
+    map,
+    mapDate,
+    key,
+  };
+};
 
 module.exports = mapLogic;
